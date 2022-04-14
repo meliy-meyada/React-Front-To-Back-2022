e7bc14a7d79a1ce6806c28e7584e024b64ba1cdb
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-15 06:10:38 +0700

    Add Function handleDelete

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,18 +5,27 @@ import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import Card from "./components/shared/Card"
 
+
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id ))
+            
+        }
+    }
+
     return (
-        <div>
+        <>
         <Header/>
             <div className="container">
-                <FeedbackList feedback={feedback} />
-                
-
+                <FeedbackList feedback={feedback}
+                 handleDelete={deleteFeedback}
+                />
+            
             </div>
-        </div>
+        </>
     )
 }
 
