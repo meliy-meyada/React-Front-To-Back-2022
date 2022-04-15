3b7767ee0b0305859e809f56e26c87cfda3827cc
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-15 14:57:46 +0700

    Create RatingSelect and set value rating

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import {useState, React} from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [reting, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
     
@@ -27,7 +29,7 @@ function FeedbackForm() {
       <Card>
           <from>
               <h2>How would you rate your service with us?</h2>
-              {/*@TODO - Rating select component*/}
+              <RatingSelect select={(rating) => setRating(rating)} />
               <div className="input-group">
                   <input
                       onChange={handleTextChange}
