08941b884db9561ae27327dda383c913e3f2073d
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-16 13:17:44 +0700

    Create AboutPages and use framework api react-router-dom@5.3.0

---
+++ feedback-app/src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from '../components/shared/Card'
+
+function AboutPage() {
+  return (
+    <Card>
+          <div className="about">
+              <h1>About This Project</h1>
+              <p>This is React app to leave feedback for a product or service</p>
+              <p>Version 1.0.0</p>
+          </div>
+          <p>
+              <a href="/">Back to Home</a>
+          </p>
+    </Card>
+  )
+}
+
+export default AboutPage
