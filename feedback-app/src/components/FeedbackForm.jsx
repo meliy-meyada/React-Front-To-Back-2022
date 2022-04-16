ca9a64fee8f9b5a30a5de03512b255b0f82ff8ac
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-16 17:41:45 +0700

    Useful libray useEffect and Clean Code

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,4 +1,4 @@
-import {useState, React, useContext} from 'react'
+import {useState, React, useContext, useEffect} from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
@@ -12,7 +12,17 @@ function FeedbackForm() {
     const [message, setMessage] = useState('')
 
 
-    const {addFeedback} = useContext(FeedbackContext)
+    const { addFeedback, feedbackEdit } = useContext(FeedbackContext)
+    
+
+    useEffect(() => {
+        if (feedbackEdit.edit === true) {
+            setBtnDisabled(false)
+            setText(feedbackEdit.item.text)
+            setRating(feedbackEdit.item.rating)
+       }
+
+    }, [feedbackEdit])
     
     const handleTextChange = (e) => {
         if (text === '') {
--- feedback-app/src/components/context/FeedbackContext.js
+++ feedback-app/src/components/context/FeedbackContext.js
@@ -52,6 +52,8 @@ export const FeedbackProvider = ({ children }) => {
         deleteFeedback,
         addFeedback,
         editFeedback,
+        feedbackEdit,
+        
     }}>
         {children}
     </FeedbackContext.Provider>
