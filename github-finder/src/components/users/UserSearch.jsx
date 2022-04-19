f4ab821a020e6cc46a2849acd58bc3f96475fd68
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-19 12:08:37 +0700

    Update UI Githubfinder using tailwindCSS DaisyUI

---
+++ github-finder/src/components/users/UserSearch.jsx
@@ -0,0 +1,60 @@
+import { useState, useContext } from "react"
+import GithubContext from "../../context/github/GithubContext"
+
+
+function UserSearch() {
+    const [text, setText] = useState('')
+
+    const {users, searchUsers} = useContext(GithubContext)
+
+    const handleChange = (e) => setText(e.target.value)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (text === '') {
+            alert("กรุณาป้อนบางสิ่งบางอย่าง")
+        } else {
+            searchUsers(text)
+            
+            setText('')
+        }
+    }
+
+  return (
+      <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+          <div>
+                <form onSubmit={handleSubmit}>
+                  <div className="form-control">
+                      <div className="relative">
+                          <input
+                              type="text"
+                              className="w-full pr-40 bg-gray-200 input input-lg text-black "
+                              placeholder="ค้นหา"
+                              value={text}
+                              onChange={handleChange}
+                          />
+                          <button
+                              type="submit"
+                              className="absolute bg-base-300 top-0 right-0 rounded-l-none w-36 btn btn-lg ">
+                              Go
+                          </button>
+                        </div>
+                  </div> 
+                </form>
+          </div>
+          <div>
+              {users.length > 0 && (
+              <button
+                  className="btn btn-ghost btn-lg">
+                  <div>
+                      Clear
+                  </div>
+              </button>
+                  
+              )}
+          </div>
+      </div>
+  )
+}
+
+export default UserSearch
