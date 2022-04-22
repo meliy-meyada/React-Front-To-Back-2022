e485cad58174100e212565ca0898023b01f556e6
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-04-22 14:00:10 +0700

    Create Project react-sandbox for my idea generate UI Example 1

---
+++ react-sandbox/src/App.js
@@ -0,0 +1,12 @@
+import UseRefExample1 from "./component/UseRefExample1"
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <UseRefExample1 />
+          
+    </div>
+  )
+}
+
+export default App
